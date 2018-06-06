293d02f18a8928ea692d24396a4297d1c3128c66
endlessai <39980868+endlessai@users.noreply.github.com>
2018-06-06 12:54:02 -0400

    Add files via upload

---
+++ Discord.py
@@ -0,0 +1,44 @@
+import discord
+import asyncio
+import random
+import pickle
+import os
+from chatterbot.trainers import ListTrainer #method to train the bot
+from chatterbot import ChatBot # import the chat bot
+
+client = discord.Client()
+
+bot = ChatBot('Test') #creates chatbot
+
+bot.set_trainer(ListTrainer) # set the trainer
+
+for _file in os.listdir('files'):
+    chats = open('files/' +_file, 'r').readlines()
+    if not os.path.isfile('files/' +_file):
+
+        bot.train(chats)
+
+while True:
+
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('!chat'):
+                new_input = message.content[6:]
+                print(new_input)
+                response = bot.get_response(new_input)
+                msg = response
+                await client.send_message(message.channel, msg)
+                print(response)
+
+    client.run(os.getenv('TOKEN'))
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# endless
+# main
+Endless AI
